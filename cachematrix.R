98f79fd4695608ea93d0ebb1624ea7446442d4a4
Dramaticlly <steveiszhy@gmail.com>
2016-01-17 23:13:30 -0500

    tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+#is.matrix returns TRUE if x is a vector and has a "dim" attribute of length 2
+# and FALSE otherwise
+
+# func to get inverse of matrix m : solve(m)
+# verify by m %*% solve(m) =  diag(nrow = nrow(m), ncol = ncol(m))
 
 makeCacheMatrix <- function(x = matrix()) {
+	inverse <- NULL
+	set <- function(y){
+		x <- y
+		inverse <- NULL
+	}
 
+	get <- function(){
+		x
+	}
+
+	setinv <- function(inv){
+		inverse <<-inv
+	}
+
+	getinv <- function(){
+		inverse
+	}
+
+	list(set 	= set,
+		 get 	= get,
+		 setinv = setinv,
+		 getinv = getinv)
 }
 
 
@@ -12,4 +38,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinv()
+        # validare inverse is not NULL
+        if(!is.null(inverse)){
+        	message("getting cached data")
+        	return(inverse)
+        }
+        # if inverse is NULL, compute
+        data <- x$get()
+        inverse <- solve(data,...)
+        x$setinv(inverse)
+        return(inverse)
 }
